c7c20628e695cc84f78fd3436e20627365280cee
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2023-10-28 23:26:23 +0200

    Remove comments

--- src/boot/buzzer.ts
+++ src/boot/buzzer.ts
@@ -1,9 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import Buzzer from 'src/plugins/buzzer';
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(({ app }) => {
-  // TODO Provide buzzer API
   app.provide('buzzer', Buzzer());
 });

543794d2e928dedbecb1d31a2f9fc9901f39308f
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-01-29 21:16:35 +0100

    Update icons

--- quasar.config.js
+++ quasar.config.js
@@ -100,7 +100,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
       config: {
-        dark: true,
+        dark: 'auto',
       },
 
       // iconSet: 'material-icons', // Quasar icon set

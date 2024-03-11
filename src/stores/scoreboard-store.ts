66dafb97be22ab24bef06bb1a7b4bd17f5362e74
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-03-11 04:46:09 +0100

    Simplify code

--- src/stores/scoreboard-store.ts
+++ src/stores/scoreboard-store.ts
@@ -15,38 +15,29 @@ export const useScoreboardStore = defineStore('scoreboard', () => {
   const points = ref<Record<string, number>>({});
 
   const scores = computed<Score[]>(() => {
-    const s = controllers.value
-      .map((controller) => {
-        return {
-          id: controller.id,
-          name: controller.name,
-          value: points.value[controller.id] ?? 0,
-          position: 0,
-        };
-      })
+    const scores = controllers.value
+      .map((controller) => ({
+        id: controller.id,
+        name: controller.name,
+        value: points.value[controller.id] ?? 0,
+        position: 0,
+      }))
       .sort((a, b) => b.value - a.value);
 
-    s.forEach((item, index) => {
-      if (index === 0) {
-        item.position = 1;
-        return;
-      }
-
-      const prev = s[index - 1];
-      if (item.value === prev.value) {
-        item.position = prev.position;
-        return;
-      }
-
-      item.position = index + 1;
+    // Set the position for each element
+    scores.forEach((score, index) => {
+      // Same position for entries with same value
+      score.position =
+        index === 0 || score.value !== scores[index - 1].value
+          ? index + 1
+          : scores[index - 1].position;
     });
 
-    return s;
+    return scores;
   });
 
   const addPoints = (controllerId: string, p: number) => {
     points.value[controllerId] ??= 0;
-
     points.value[controllerId] += p;
   };
 

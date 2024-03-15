1a8825c24f429778add6315d7bd84fbfcb172c16
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-03-15 22:32:25 +0100

    Improve countdown time tracking

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -13,12 +13,10 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onBeforeMount, onUnmounted, ref, watch } from 'vue';
+import { computed, onBeforeMount, onUnmounted, watch } from 'vue';
 
 interface Props {
-  modelValue?: number;
   paused?: boolean;
-  time?: number;
   reverse?: boolean;
   updateRate?: number;
   beep?: boolean;
@@ -27,10 +25,13 @@ interface Props {
   animated?: boolean;
 }
 
+const time = defineModel<number>({
+  default: 0,
+});
+
 const props = withDefaults(defineProps<Props>(), {
   paused: false,
   reverse: false,
-  time: 10,
   updateRate: 10,
   beep: false,
   beepStartTime: 10,
@@ -39,42 +40,33 @@ const props = withDefaults(defineProps<Props>(), {
 });
 
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: number): void;
+  (e: 'start'): void;
+  (e: 'stop'): void;
 }>();
 
 const shortBeep = new Audio('sounds/beep-sec.mp3');
 const longBeep = new Audio('sounds/beep-end.mp3');
+let intervalId: NodeJS.Timeout | undefined;
 
-watch(
-  () => props.paused,
-  (paused) => {
-    // Always stop to make sure only one countdown is running
-    stop();
-    if (!paused) {
-      start();
-    }
-  },
-);
+watch([props.paused, props.reverse], ([paused]) => {
+  // Always stop to make sure only one countdown is running
+  stop();
+  if (!paused) {
+    start();
+  }
+});
 
 onBeforeMount(() => {
+  loadAudio();
+
   if (!props.paused) {
     start();
-    value.value = props.time ?? 0;
   }
-
-  loadAudio();
 });
 
 onUnmounted(() => {
   stop();
 });
-
-const time = computed<number>(() => {
-  return props.modelValue ?? value.value;
-});
-
-const value = ref<number>(0);
-let intervalId: NodeJS.Timeout | undefined;
 
 const displayTime = computed<string>(() => {
   const factor = Math.pow(10, props.precision);
@@ -97,11 +89,16 @@ const stop = () => {
     clearInterval(intervalId);
   }
   intervalId = undefined;
+  emit('stop');
 };
 
 const start = () => {
   const timeout = 1000 / props.updateRate;
   const interval = 1 / props.updateRate;
+
+  const initialTime = time.value;
+  const startTimestamp = new Date().getTime();
+  const reverse = props.reverse ? -1 : 1;
 
   intervalId = setInterval(() => {
     if (time.value <= 0 && !props.reverse) {
@@ -114,7 +111,7 @@ const start = () => {
       return;
     }
 
-    const nextTime = time.value - interval * (props.reverse ? -1 : 1);
+    const nextTime = time.value - interval * reverse;
 
     // Only beep on falling flanks. Flanks from one to zero need to ignored as it is already the last one
     if (
@@ -126,15 +123,18 @@ const start = () => {
       shortBeep.play();
     }
 
-    value.value = nextTime;
-    emit('update:modelValue', nextTime);
+    const now = new Date().getTime();
+    const elapsedTime = now - startTimestamp;
+
+    time.value = initialTime - elapsedTime * reverse;
   }, timeout);
+
+  emit('start');
 };
 
 const formatTime = (time: number) => {
   const date = new Date(time * 1000);
 
-  // Seconds
   let timeString = '';
 
   // Minutes
@@ -143,6 +143,7 @@ const formatTime = (time: number) => {
     timeString = `${minutes}:`;
   }
 
+  // Seconds
   const seconds = String(date.getUTCSeconds()).padStart(2, '0');
   timeString += `${seconds}`;
 

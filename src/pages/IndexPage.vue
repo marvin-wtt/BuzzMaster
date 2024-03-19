66396c6d4ae0d59002f596e2f535fc690a149661
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-03-19 17:53:55 +0100

    Fix version style

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,34 +1,36 @@
 <template>
-  <q-page class="column items-center justify-evenly">
-    <a class="text-h3">
-      {{ t('app_name') }}
-    </a>
+  <q-page class="column justify-between">
+    <div class="col-grow column items-center justify-evenly">
+      <a class="text-h3">
+        {{ t('app_name') }}
+      </a>
 
-    <q-img
-      src="logo.png"
-      alt="logo"
-      style="width: 250px"
-    />
-
-    <div class="column q-gutter-lg">
-      <q-btn
-        :label="t('action.start')"
-        icon="play_arrow"
-        to="questions"
-        color="primary"
-        rounded
+      <q-img
+        src="logo.png"
+        alt="logo"
+        style="width: 250px"
       />
 
-      <q-btn
-        :label="t('action.devices')"
-        icon="sports_esports"
-        to="devices"
-        color="primary"
-        rounded
-      />
+      <div class="column q-gutter-lg">
+        <q-btn
+          :label="t('action.start')"
+          icon="play_arrow"
+          to="questions"
+          color="primary"
+          rounded
+        />
+
+        <q-btn
+          :label="t('action.devices')"
+          icon="sports_esports"
+          to="devices"
+          color="primary"
+          rounded
+        />
+      </div>
     </div>
 
-    <div>v{{ version }}</div>
+    <div class="self-center q-pa-sm">v{{ version }}</div>
   </q-page>
 </template>
 

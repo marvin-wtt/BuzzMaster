91c98156d93087c519e17c7dfbdb6cc50d1deb54
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2024-04-20 00:15:39 +0200

    Feat: Throw error oon illegal transition

--- src/stores/game-store.ts
+++ src/stores/game-store.ts
@@ -2,15 +2,24 @@ import { acceptHMRUpdate, defineStore } from 'pinia';
 import { ref } from 'vue';
 import { GameState } from 'app/common/gameState';
 
+class StateTransitionError extends Error {
+  constructor(state: GameState, prevState: GameState) {
+    super(`Invalid transition from ${prevState.game} to ${state.game}`);
+  }
+}
+
 export const useGameStore = defineStore('game-store', () => {
   const state = ref<GameState>();
 
   function transition(gameState: GameState) {
-    if (state.value !== undefined && state.value?.name !== gameState.name) {
+    if (state.value !== undefined && state.value?.game !== gameState.game) {
+      const prevState = state.value;
       // This should be an illegal transition.
       // Transitions between games should always be after a reset.
       // Reset it manually to allow state triggers to take action.
       state.value = undefined;
+
+      throw new StateTransitionError(prevState, gameState);
     }
 
     state.value = { ...gameState };

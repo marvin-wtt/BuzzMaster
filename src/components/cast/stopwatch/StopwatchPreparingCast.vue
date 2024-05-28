c57827b5ed320b49f67cfaf4b150e58de8bffacb
McMoerlin <137455539+McMoerlin@users.noreply.github.com>
2024-05-28 02:14:52 +0200

    change logo size stopwatch cast

--- src/components/cast/stopwatch/StopwatchPreparingCast.vue
+++ src/components/cast/stopwatch/StopwatchPreparingCast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="column justify-center col-11">
+  <div class="column justify-center col-sm-8 col-md-5 col-lg-3 col-xl-2">
     <div class="text-center text-h2 q-pa-lg">
       {{ t('cast.stopwatch.preparing.title') }}
     </div>

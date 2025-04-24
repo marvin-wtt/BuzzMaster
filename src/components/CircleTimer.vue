56cfd35323cee5a3395145270fc48cca707e5cf1
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-04-24 05:23:37 +0200

    feat: Replace circular timer with quasar component

--- src/components/CircleTimer.vue
+++ src/components/CircleTimer.vue
@@ -1,123 +1,49 @@
 <template>
-  <!-- TODO Can be replace by q-circular-progress -->
-  <div class="base-timer">
-    <svg
-      class="base-timer__svg"
-      viewBox="0 0 100 100"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g class="base-timer__circle">
-        <circle
-          class="base-timer__path-elapsed"
-          cx="50"
-          cy="50"
-          r="45"
-        />
-        <path
-          :stroke-dasharray="strokeDashArray"
-          :class="colorClass"
-          class="base-timer__path-remaining"
-          d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-        />
-      </g>
-    </svg>
-    <span class="base-timer__label">
-      <slot />
-    </span>
-  </div>
+  <q-circular-progress
+    :value="time"
+    :max
+    :color
+    :thickness
+    show-value
+    class="fit"
+  >
+    <slot />
+  </q-circular-progress>
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue';
 
-interface Props {
+const {
+  time,
+  max,
+  thickness = 0.1,
+  infoColor = 'text',
+  warningColor = 'warning',
+  warningThreshold = 5,
+  alertColor = 'negative',
+  alertThreshold = 3,
+} = defineProps<{
   time: number;
   max: number;
-  width?: string | number;
-  infoClass?: string;
-  warningClass?: string;
+  thickness?: number;
+  infoColor?: string;
+  warningColor?: string;
   warningThreshold?: number;
-  alertClass?: string;
+  alertColor?: string;
   alertThreshold?: number;
-}
+}>();
 
-const props = withDefaults(defineProps<Props>(), {
-  width: '5px',
-  infoClass: 'text',
-  warningClass: 'text-warning',
-  warningThreshold: 5,
-  alertClass: 'text-negative',
-  alertThreshold: 3,
-});
-
-const colorClass = computed<string>(() => {
-  if (time.value <= props.alertThreshold) {
-    return props.alertClass;
+const color = computed<string>(() => {
+  if (time <= alertThreshold) {
+    return alertColor;
   }
-  if (time.value <= props.warningThreshold) {
-    return props.warningClass;
+  if (time <= warningThreshold) {
+    return warningColor;
   }
 
-  return props.infoClass;
-});
-
-const time = computed<number>(() => {
-  return Math.ceil(props.time);
-});
-
-const timeFraction = computed(() => {
-  const rawTimeFraction = time.value / props.max;
-  return rawTimeFraction - (1 / props.max) * (1 - rawTimeFraction);
-});
-
-const strokeDashArray = computed(() => {
-  return `${(timeFraction.value * 283).toFixed(0)} 283`;
+  return infoColor;
 });
 </script>
 
-<style lang="scss" scoped>
-.base-timer {
-  position: relative;
-  width: 100%;
-  aspect-ratio: 1 / 1;
-}
-
-.base-timer__svg {
-  transform: scaleX(-1);
-}
-
-.base-timer__circle {
-  fill: none;
-  stroke: none;
-}
-
-.base-timer__path-elapsed {
-  stroke-width: v-bind(width);
-  stroke: grey;
-}
-
-.base-timer__path-remaining {
-  stroke-width: v-bind(width);
-  stroke-linecap: round;
-  transform: rotate(90deg);
-  transform-origin: center;
-  transition: 1s linear all;
-  fill-rule: nonzero;
-  stroke: currentColor;
-}
-
-.base-timer__label {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
+<style scoped></style>

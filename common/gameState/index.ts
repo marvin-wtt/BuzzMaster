c299ed0b8c73249413138a4952bd1979bd80e9ac
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2025-05-14 06:30:36 +0200

    refactor: Fix type imports

--- common/gameState/index.ts
+++ common/gameState/index.ts
@@ -1,8 +1,8 @@
-import { BuzzerState } from 'app/common/gameState/BuzzerState';
-import { QuizState } from 'app/common/gameState/QuizState';
-import { StopwatchState } from 'app/common/gameState/StopwatchState';
-import { LeaderboardState } from 'app/common/gameState/LeaderboardState';
-import { ViewingRateState } from 'app/common/gameState/ViewingRateState';
+import type { BuzzerState } from 'app/common/gameState/BuzzerState';
+import type { QuizState } from 'app/common/gameState/QuizState';
+import type { StopwatchState } from 'app/common/gameState/StopwatchState';
+import type { LeaderboardState } from 'app/common/gameState/LeaderboardState';
+import type { ViewingRateState } from 'app/common/gameState/ViewingRateState';
 
 export type GameState =
   | BuzzerState

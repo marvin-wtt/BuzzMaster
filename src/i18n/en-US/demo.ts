ae26e60d381172642accd5febb2908118e273a52
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2026-02-18 04:37:15 +0100

    fix: improve wording in demo message for clarity

--- src/i18n/en-US/demo.ts
+++ src/i18n/en-US/demo.ts
@@ -1,7 +1,7 @@
 export default {
   title: '@:app_name Demo',
   message:
-    "This is a demo version of @:{'app_name'} with limited features. We recommend you to download the latest version instead.",
+    "This is a demo version of @:{'app_name'} with limited features. We recommend downloading the latest version instead.",
   permissions:
     'In order to use the demo version, you need to grant permissions to all buzzer devices.',
   help: 'If the device is not detected by your browser, please check the devices page for troubleshooting.',
